d566e67b6f39362e659d00dd0acd1ffec2d4f7c5
Asim Ehmed <rocco.borocco95@gmail.com>
2025-09-24 13:55:21 +0400

    Screen settings and turtle added

---
+++ CatchTheTurtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+#Screen settings
+wn = turtle.Screen()
+wn.bgcolor("lightblue")
+wn.title("Catch the Turtle")
+wn.setup(width=800, height=600)
+
+#Turtle
+catchTheTurtle = turtle.Turtle()
+catchTheTurtle.shape("turtle")
+catchTheTurtle.color("green")
+
+
+
+wn.mainloop()
